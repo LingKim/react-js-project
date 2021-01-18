6691005007477e7f82a32047dd34dc6ae1fd3e24
lilin <1660312954@qq.com>
2021-01-18 13:51:22 +0800

    测试git是否生效

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Routes from "./routes"
 import { BrowserRouter } from 'react-router-dom'
 
+
 const App = () => {
 	return (
 		<BrowserRouter basename="/">{Routes()}</BrowserRouter>
